52d5b7818bec2882eafec1952e1bcf61baa06d02
Devinandan <161704479+Devinandan@users.noreply.github.com>
2024-10-18 18:58:55 +0530

    buttons done

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function(){
     
-    createBoard(32);
-    console.log("hi");
+    createBoard(16);
+    
+    let btn_popUp = document.querySelector("#popUp");
+    btn_popUp.addEventListener("click", function(){
+        let s = getSize();
+        if( s !== null){
+            createBoard(s);
+        }
+    })
+    
 })
 
 function createBoard(size) {
@@ -14,7 +24,47 @@ function createBoard(size) {
 
     for( i=0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv)
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function getSize() {
+    let input = prompt("Enter the size of the board :");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+        return null;
+    }
+    let size = parseInt(input);
+    if(isNaN(size) || size < 0 || size > 100){
+        message.innerHTML = "Enter a number between 1 and 100";
+        return null;
+    }
+    else{
+        message.innerHTML = "Correct input!";
+        return size;
+        
+    }
+    
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else{
+        this.style.backgroundColor = "black";
+    }
+
+
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
